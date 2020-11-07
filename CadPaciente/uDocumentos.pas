181e5cea009b80204b19f3f0a3d505ea6a0bfe41
TerenceSSantos <terence.teo2@gmail.com>
2020-11-07 20:02:04 -0300

    Começando inserção de Documentos 2

--- CadPaciente/uDocumentos.pas
+++ CadPaciente/uDocumentos.pas
@@ -13,6 +13,7 @@ type
 
    Documentos = class
      class function CarregaObjDocumentos(objDocumentos: TDocumentos; frm: TFrmCadPaciente): TDocumentos;
+     class procedure InclusaoOuEdicaoDocumentos(frm: TfrmCadPaciente);
    end;
 
 implementation
@@ -27,6 +28,34 @@ begin
       orgaoExpedidor := frm.edtOrgaoExpedPaciente.Text;
       cpf := frm.mskedtCPFPaciente.Text;
       idTblPaciente := StrToInt(frm.edtCodPaciente.Text);
+   end;
+   result := objDocumentos;
+end;
+
+class procedure Documentos.InclusaoOuEdicaoDocumentos(frm: TfrmCadPaciente);
+var
+  objDocumentos : TDocumentos;
+  objControlePaciente : TControlePaciente;
+begin
+   try
+      objDocumentos := TEndereco.Create;
+      objControlePaciente := TControlePaciente.Create;
+      if objControlePaciente.InclusaoEndereco(CarregaObjEndereco(objDocumentos, frm)) then
+       begin
+          try
+             frmMensagem := TfrmMensagem.Create(nil);
+             frmMensagem.InfoFormMensagem('Cadastro de Documentos', tiInformacao, 'Gravação dos Documentos realizado com sucesso!');
+          finally
+             FreeAndNil(frmMensagem);
+          end;
+       end;
+
+      //DesabilitaControles(pcCadPaciente.ActivePage);
+      estado := teNavegacao;
+      //EstadoBotoes;
+   finally
+      FreeAndNil(objControlePaciente);
+      FreeAndNil(objDocumentos);
    end;
 end;
 
